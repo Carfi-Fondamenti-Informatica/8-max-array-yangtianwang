9869971ef031bb95566240e38b1d0551bfac833b
zhuoxiao yang <91831716+yangtianwang@users.noreply.github.com>
2021-12-15 21:00:55 +0800

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,5 +1,17 @@
-#ifndef UNTITLED15_LIB_H
-#define UNTITLED15_LIB_H
-float numeroMassimo(const float numeri[], int dimensione);
 
-#endif //UNTITLED15_LIB_H
+#include "lib.h"
+
+float numeroMassimo(const float numeri[], int dim){
+    numeri [dim] ;
+    
+   float massimo = numeri[0];
+    
+  for (int i=0; i < dim; i++){
+    if(massimo < numeri[i]){
+      massimo = numeri[i];
+       }
+      
+}
+return massimo;
+    
+}
