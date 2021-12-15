2b54ade8e94ad837994dcb4480c520f46858e4ae
zhuoxiao yang <91831716+yangtianwang@users.noreply.github.com>
2021-12-15 21:01:49 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
+using namespace std;
+
+int main() {
+  int dim;
+  cin >> dim;
+	
+  float numeri[dim];
+	
+  for (int i = 0; i < dim; i++ ){
+    cin >> numeri[i];
+  }
+  numeroMassimo(numeri, dim);
+  
+    cout << numeroMassimo(numeri , dim) << endl;
+  
   return 0;
-}
+	      }
